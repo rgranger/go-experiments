bc12d95c9e21aa3a430a3566137a7a4b6fcf0175
Robin Granger <robin.granger@analogway.com>
2021-02-19 16:12:09 +0100

    evolution(resolvers) : ajoute l'implémentation de la mutation CreateLink

--- src/backend/graph/schema.resolvers.go
+++ src/backend/graph/schema.resolvers.go
@@ -12,7 +12,13 @@ import (
 )
 
 func (r *mutationResolver) CreateLink(ctx context.Context, input model.NewLink) (*model.Link, error) {
-	panic(fmt.Errorf("not implemented"))
+	var link model.Link
+	var user model.User
+	link.Address = input.Address
+	link.Title = input.Title
+	user.Name = "test"
+	link.User = &user
+	return &link, nil
 }
 
 func (r *mutationResolver) CreateUser(ctx context.Context, input model.NewUser) (string, error) {
@@ -30,13 +36,13 @@ func (r *mutationResolver) RefreshToken(ctx context.Context, input model.Refresh
 func (r *queryResolver) Links(ctx context.Context) ([]*model.Link, error) {
 	var links []*model.Link
 	dummyLink := model.Link{
-	  Title: "our dummy link",
-	  Address: "https://address.org",
-	  User: &model.User{Name: "admin"},
+		Title:   "our dummy link",
+		Address: "https://address.org",
+		User:    &model.User{Name: "admin"},
 	}
-	  links = append(links, &dummyLink)
-	  return links, nil
-  }
+	links = append(links, &dummyLink)
+	return links, nil
+}
 
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
